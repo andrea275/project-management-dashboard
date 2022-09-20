c3741ee6dc6c4afb83e17062c73b7479c7a2cfcc
andrea275 <andrea.vujisic27@gmail.com>
2022-09-20 19:47:01 +0200

    Log out button

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -56,12 +56,17 @@
                 @endif
             </li>
             <li>
-                <a href="{{ route('logout') }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-200">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <span onclick="event.preventDefault();
+                                        this.closest('form').submit();" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-200 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                     </svg>
                     <span class="flex-1 ml-3 whitespace-nowrap">Log Out</span>
-                </a>
+                </span>
+                </form>
             </li>
         </ul>
     </div>
